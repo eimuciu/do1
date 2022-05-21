376423d155b7bb847a2b0e8760ee5fba5c1bd872
ed <allgoodata@outlook.com>
2022-05-21 11:24:58 +0300

    minor changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end('Hello, World!');
+  res.end('<h1>Hello, World!</h1>');
 };
 
 const server = http.createServer(requestListener);
